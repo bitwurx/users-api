cadd640e504526d383e4c3b4cf313f7d1e608171
Jared Patrick <jared.patrick@gmail.com>
2016-10-26 05:50:32 +0000

    added api test unit tests

--- test/test_api.py
+++ test/test_api.py
@@ -1,3 +1,5 @@
+import json
+
 from unittest import mock
 
 from arango.exceptions import (
@@ -6,6 +8,9 @@ from arango.exceptions import (
 )
 
 from users.api import connect_arango
+from users.api import (
+    UsersResourceV1
+)
 
 
 @mock.patch('users.api.arango.ArangoClient')
@@ -41,8 +46,8 @@ def test_connect_arango_handles_existing_database_exception(MockArangoClient):
 @mock.patch('users.api.arango.ArangoClient')
 def test_connect_arango_creates_collection(MockArangoClient):
     connect_arango()
-    mock_db = MockArangoClient().create_database()
-    mock_db.create_collection.assert_called_with('users')
+    db = MockArangoClient().create_database()
+    db.create_collection.assert_called_with('users')
 
 
 @mock.patch('users.api.arango.ArangoClient')
@@ -51,7 +56,46 @@ def test_connect_arango_handle_existing_collection_exception(MockArangoClient):
         if name == 'users':
             raise CollectionCreateError(None)
 
-    mock_db = MockArangoClient().create_database()
-    mock_db.create_collection.side_effect = collection_exists
+    db = MockArangoClient().create_database()
+    db.create_collection.side_effect = collection_exists
     connect_arango()
-    mock_db.collection.assert_called_with('users')
+    db.collection.assert_called_with('users')
+
+
+@mock.patch('users.api.bcrypt.gensalt')
+@mock.patch('users.api.users')
+def test_UsersResource_post_creates_user_document(mock_users, mock_gensalt):
+    user = {'username': 'joe', 'password': 'test', 'email': 'joe@schmoe.com'}
+    mock_gensalt.return_value = b'$2b$12$buE1MyflUVhDp92MkjQV3O'
+    request = mock.Mock()
+    request.body = json.dumps(user).encode()
+    resource = UsersResourceV1(mock.MagicMock(), mock.Mock())
+    resource.request = request
+    resource.post()
+    pwhash = b'$2b$12$buE1MyflUVhDp92MkjQV3OqXCVBhtMZk1bUywn6dTTBmTgy4WbEpe'
+    mock_users.insert.assert_called_with({'username': 'joe',
+                                          'password': pwhash,
+                                          'email': 'joe@schmoe.com'})
+
+
+@mock.patch('users.api.bcrypt.gensalt')
+@mock.patch('users.api.cerberus.Validator')
+def test_UsersResource_post_validates_request_data(
+        MockValidator,
+        mock_gensalt):
+    mock_gensalt.return_value = b'$2b$12$buE1MyflUVhDp92MkjQV3O'
+    user = {'username': 'joe', 'password': 'test', 'email': 'joe@schmoe.com'}
+    resource = UsersResourceV1(mock.MagicMock(), mock.Mock())
+    resource.request.body = json.dumps(user).encode()
+    resource.post()
+    assert resource.schema == {
+        'username': {'type': 'string'},
+        'password': {'type': 'string'},
+        'email': {'type': 'string'}
+    }
+    pwhash = b'$2b$12$buE1MyflUVhDp92MkjQV3OqXCVBhtMZk1bUywn6dTTBmTgy4WbEpe'
+    MockValidator().validate.assert_called_with({
+        'email': user['email'],
+        'username': user['username'],
+        'password': pwhash
+    }, resource.schema)
--- users/api.py
+++ users/api.py
@@ -3,10 +3,18 @@
 .. moduleauthor:: Jared Patrick <jared.patrick@gmail.com>
 """
 
+import json
 import os
 
 import arango
 import arango.exceptions
+
+import bcrypt
+
+import cerberus
+
+import tornado.gen
+import tornado.web
 
 
 users = None
@@ -36,3 +44,28 @@ def connect_arango():
         users = db.create_collection('users')
     except arango.exceptions.CollectionCreateError:
         users = db.collection('users')
+
+
+class UsersResourceV1(tornado.web.RequestHandler):
+    schema = {
+        'username': {'type': 'string'},
+        'password': {'type': 'string'},
+        'email': {'type': 'string'}
+    }
+
+    @tornado.gen.coroutine
+    def post(self):
+        """Create a new user document
+        """
+
+        global users
+
+        body = json.loads(self.request.body.decode())
+        body['password'] = bcrypt.hashpw(body['password'].encode(),
+                                         bcrypt.gensalt())
+        cerberus.Validator().validate(body, self.schema)
+        users.insert({
+            'username': body['username'],
+            'password': body['password'],
+            'email': body['email']
+        })
